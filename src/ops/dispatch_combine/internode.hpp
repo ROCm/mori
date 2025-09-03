0e92aaf8930232f07e8abfd848dd414b7d999a83
Wu, Yutong <yutong.wu@amd.com>
2025-09-03 17:47:20 +0800

    Fix: reduce memory overuse and fix poll cqe hang (#60)
    *  poll cqe hang WA
    
    * fix internode segFault
    
    * reduce memory overuse
    
    * add ShmemQuietThread in dispatch
    
    ---------
    
    Co-authored-by: jhchouuu <jiahzhou@amd.com>

--- src/ops/dispatch_combine/internode.hpp
+++ src/ops/dispatch_combine/internode.hpp
@@ -314,7 +314,9 @@ __global__ void EpDispatchInterNodeKernel(EpDispatchCombineArgs<T> args) {
       args.srcPeTokenIdxMap[peSortedId] = localTokenIdx;
     }
   }
-  SyncIfDebugEnabled("Dispatch kernel: kernel end");
+  shmem::ShmemQuietThread();
+  __syncthreads();
+  // SyncIfDebugEnabled("Dispatch kernel: kernel end");
 }
 
 /* ---------------------------------------------------------------------------------------------- */
@@ -483,10 +485,7 @@ __global__ void EpCombineInterNodeKernel(EpDispatchCombineArgs<T> args) {
 
   // Make sure copy on all GPUs are finished
   CrossDeviceBarrierInterNodeKernel(args);
-  if (localBlockId == 0 && warpId == 0) {
-    shmem::ShmemQuietThread(srcPe);
-  }
-  
+  shmem::ShmemQuietThread();
   if (globalThdId < npes) {
     args.recvTokenNumMemObj->template GetAs<index_t*>()[globalThdId] = 0;
   }
