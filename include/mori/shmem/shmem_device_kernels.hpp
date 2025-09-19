32608aa795e2ffa08a1be6e51be3cd0ba5a5cb00
jhchouuu <jiahzhou@amd.com>
2025-09-19 16:26:00 +0800

    fix compile error and test done

--- include/mori/shmem/shmem_device_kernels.hpp
+++ include/mori/shmem/shmem_device_kernels.hpp
@@ -33,51 +33,53 @@ template <application::TransportType TsptType>
 inline __device__ void ShmemPutMemNbiThreadKernel(const application::SymmMemObjPtr dest,
                                                   size_t destOffset,
                                                   const application::RdmaMemoryRegion& source,
-                                                  size_t sourceOffset, size_t bytes, int pe);
+                                                  size_t sourceOffset, size_t bytes, int pe,
+                                                  int qpId = 0);
 
 template <application::TransportType TsptType>
 inline __device__ void ShmemPutMemNbiWarpKernel(const application::SymmMemObjPtr dest,
                                                 size_t destOffset,
                                                 const application::RdmaMemoryRegion& source,
-                                                size_t sourceOffset, size_t bytes, int pe);
+                                                size_t sourceOffset, size_t bytes, int pe,
+                                                int qpId = 0);
 
 template <application::TransportType TsptType>
 inline __device__ void ShmemPutSizeImmNbiThreadKernel(const application::SymmMemObjPtr dest,
                                                       size_t destOffset, void* val, size_t bytes,
-                                                      int pe);
+                                                      int pe, int qpId = 0);
 
 template <application::TransportType TsptType>
 inline __device__ void ShmemPutSizeImmNbiWarpKernel(const application::SymmMemObjPtr dest,
                                                     size_t destOffset, void* val, size_t bytes,
-                                                    int pe);
+                                                    int pe, int qpId = 0);
 
 template <application::TransportType TsptType>
 inline __device__ void ShmemAtomicSizeNonFetchThreadKernel(
     const application::SymmMemObjPtr dest, size_t destOffset,
     const application::RdmaMemoryRegion& source, size_t sourceOffset, void* val, size_t bytes,
-    int pe, core::atomicType amoType);
+    core::atomicType amoType, int pe, int qpId = 0);
 
 template <application::TransportType TsptType>
 inline __device__ void ShmemAtomicSizeNonFetchWarpKernel(
     const application::SymmMemObjPtr dest, size_t destOffset,
     const application::RdmaMemoryRegion& source, size_t sourceOffset, void* val, size_t bytes,
-    int pe, core::atomicType amoType);
+    core::atomicType amoType, int pe, int qpId = 0);
 
 template <application::TransportType TsptType>
 inline __device__ void ShmemAtomicSizeFetchThreadKernel(const application::SymmMemObjPtr dest,
                                                         size_t destOffset,
                                                         const application::RdmaMemoryRegion& source,
                                                         size_t sourceOffset, void* val,
-                                                        void* compare, size_t bytes, int pe,
-                                                        core::atomicType amoType);
+                                                        void* compare, size_t bytes,
+                                                        core::atomicType amoType, int pe, int qpId = 0);
 
 template <application::TransportType TsptType>
 inline __device__ void ShmemAtomicSizeFetchWarpKernel(const application::SymmMemObjPtr dest,
                                                       size_t destOffset,
                                                       const application::RdmaMemoryRegion& source,
                                                       size_t sourceOffset, void* val, void* compare,
-                                                      size_t bytes, int pe,
-                                                      core::atomicType amoType);
+                                                      size_t bytes, core::atomicType amoType,
+                                                      int pe, int qpId = 0);
 
 /* ---------------------------------------------------------------------------------------------- */
 /*                                         Synchronization                                        */
@@ -86,7 +88,7 @@ template <application::TransportType>
 inline __device__ void ShmemQuietThreadKernel();
 
 template <application::TransportType>
-inline __device__ void ShmemQuietThreadKernel(int pe);
+inline __device__ void ShmemQuietThreadKernel(int pe, int qpId = 0);
 
 }  // namespace shmem
 }  // namespace mori
