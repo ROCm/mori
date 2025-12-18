a53d99ba47958f002a3dc590823047da388b5bec
zhangfei829 <328415201@qq.com>
2025-12-18 17:56:22 +0800

    update the sdma
    Signed-off-by: zhangfei829 <328415201@qq.com>

--- examples/sdma/run_bandwidth_single_producer.sh
+++ examples/sdma/run_bandwidth_single_producer.sh
@@ -5,18 +5,19 @@ MIN_COPY_SIZE=$((1<<10))
 MAX_COPY_SIZE=$((1<<30))
 
 TIMESTAMP=$(date '+%Y-%m-%d_%Hh%Mm%Ss')
-OUTPUT_DIR="p2p_xgmi_bandwidth_$TIMESTAMP"
+OUTPUT_DIR="p2p_xgmi_bandwidth"
 SUMMARY_FILE="$OUTPUT_DIR/summary.csv"
 
-mkdir $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
 touch $SUMMARY_FILE
 
 echo "==== Running shader_sdma from $MIN_COPY_SIZE to $MAX_COPY_SIZE ===="
-rm log.txt
+rm -rf log.txt
 for (( NUM_DST=1; NUM_DST<=7; NUM_DST++ ))
 do
     RESULT_CSV="p2p_xgmi_bandwidth_${NUM_DST}dst.csv"
-    ./build/bench/sdma_bw --minCopySize $MIN_COPY_SIZE --maxCopySize $MAX_COPY_SIZE --numCopyCommands 1 --numDestinations $NUM_DST -o $OUTPUT_DIR/$RESULT_CSV  >> log.txt
+    #./build/bench/sdma_bw --minCopySize $MIN_COPY_SIZE --maxCopySize $MAX_COPY_SIZE --numCopyCommands 1 --numDestinations $NUM_DST -o $OUTPUT_DIR/$RESULT_CSV  >> log.txt
+    ../../build/examples/sdma_bw --minCopySize $MIN_COPY_SIZE --maxCopySize $MAX_COPY_SIZE --numCopyCommands 1 --numDestinations $NUM_DST
     if [ $NUM_DST -eq 1 ]; then
         cat $OUTPUT_DIR/$RESULT_CSV >> $SUMMARY_FILE 
     else
