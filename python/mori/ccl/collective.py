f5618498072b71e636dc8a4d509a65b577eeb9fd
Zhang <fizhang@amd.com>
2026-01-30 02:45:42 +0800

    update allgather
    Signed-off-by: Zhang <fizhang@amd.com>

--- python/mori/ccl/collective.py
+++ python/mori/ccl/collective.py
@@ -1,26 +1,43 @@
-import numpy as np
+# Copyright © Advanced Micro Devices, Inc. All rights reserved.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 from mori import cpp as mori_cpp
-from mori import shmem as mori_shme
+import torch
+import ctypes
 
-class All2allSdma:
-    """极简的All2All SDMA封装"""
-
+class All2allSdma: 
     def __init__(self, my_pe: int, npes: int):
         self.my_pe = my_pe
         self.npes = npes
-
-    def __call__(self,
-                 input_data: np.ndarray,
+    
+    def __call__(self, 
+                 input_data: np.ndarray, 
                  output_data: np.ndarray,
                  count: int) -> float:
-        """
-        执行All2All操作
-
+        """        
         Args:
             input_data: 输入数据 (uint32 numpy数组)
-            output_data: 输出数据 (uint32 numpy数组)
+            output_data: 输出数据 (uint32 numpy数组) 
             count: 每个PE的元素数量
-
+            
         Returns:
             执行时间(秒)
         """
