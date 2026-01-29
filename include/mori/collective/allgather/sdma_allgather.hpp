abbf3c6893048f8ae47250269d687869da6a25b3
wuyl1 <yangwu@amd.com>
2026-01-29 17:30:17 +0800

    update

--- include/mori/collective/allgather/sdma_allgather.hpp
+++ include/mori/collective/allgather/sdma_allgather.hpp
@@ -232,11 +232,11 @@ double AllGather_sdma(T* input, T* output, size_t total_count,
 
 
   if(myPe == 0){
-    double global_bandwidth = total_count * dtype_size * npes /total_c/50.0;
+    double global_bandwidth = total_count * dtype_size * npes /(total_c/50.0);
     global_bandwidth /= (1024.0 * 1024.0 * 1024.0);
 
     printf("============ avg coll time    :%0.9f    ms============= \n", total_c/50.0);
-    printf("============ avg coll bw      :%0.9f    GB/s ============= \n", global_bandwidth*100000.0);
+    printf("============ avg coll bw      :%0.9f    GB/s ============= \n", global_bandwidth);
     printf("============ avg gemm time    :%0.9f    ms============= \n", total_g/50.0);
     printf("============ avg overlap time :%0.9f    ms============= \n", total/50.0);
   }
